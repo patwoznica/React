2e21844e9a78105153c85838507121ee35217de5
Patryk Woznica <pat.woznica@gmail.com>
2021-10-03 17:50:00 +0200

    Module 11.5 Adding creator

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,61 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import {settings} from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
+	
+	state = {
+        cards: this.props.cards || [],
+    }
+
 	static propTypes = {
-		columnTitle: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      cards: PropTypes.array,
+      icon: PropTypes.node,
+      addCard: PropTypes.func,
 	}
-  render() {
-    return (
-     <section className={styles.component}>
-          <h3 className={styles.title}>{this.props.columnTitle}</h3>
+
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
+	}
+
+ addCard(title){
+     this.setState(state => (
+      {
+       cards: [
+        ...state.cards,
+        {
+        key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+        title,
+        icon: 'list all',
+        cards: []
+      }
+     ]
+    }
+ ));
+}
+
+render() {
+  return (
+	<section className={styles.component}>
+            <h3 className={styles.title}>
+               {this.props.title}
+               <span className={styles.icon}>
+                <Icon name={this.props.icon}/>
+                </span>
+                </h3>
+                <div className={styles.cards}>
+                {this.state.cards.map(({key, ...cardProps}) => (
+                <Card key={key} {...cardProps}/>
+                ))}
+                </div>
+                <div className={styles.creator}>
+                 <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+                </div>
         </section>
     )
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
-class Hero extends React.Component {
-  render() {
-    return (
+//class Hero extends React.Component {
+//  render() {
+//    return (
+//      <header className={styles.component}>
+//		<h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+//        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
+//      </header>
+//    )
+//  }
+//}
+
+const Hero = props => {
+
+    return(
       <header className={styles.component}>
-		<h2 className={styles.title}>Things to do</h2>
-        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
+        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+        <img className={styles.image} src = {props.imageLink} />
       </header>
     )
-  }
 }
 
 Hero.propTypes = {
