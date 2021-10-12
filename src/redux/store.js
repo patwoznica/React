0f871c2a8caaa6d168c790ddd3949400232043e5
Patryk Woznica <pat.woznica@gmail.com>
2021-10-12 16:05:58 +0200

    Module 12.4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import {combineReducers, createStore} from 'redux';
-import initialStoreData from '../src/data/dataStore';
+import initialStoreData from '../data/dataStore';
+import columnsReducer from './columnsRedux.js';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -11,6 +12,7 @@ const initialState = {
 
 // define reducers
 const reducers = {
+  columns: columnsReducer,
 };
 
 // add blank reducers for initial state properties without reducers
