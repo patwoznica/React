31dd4c585d39c204aded00e92c53c38c8822f109
Patryk Woznica <pat.woznica@gmail.com>
2021-10-14 11:06:55 +0200

    Fixing module 12.5

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,14 +3,10 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import {settings} from '../../data/dataStore.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-	
-//   state = {
-//     cards: this.props.cards || [],
-//   }
 
    static propTypes = {
      title: PropTypes.string,
@@ -23,7 +19,7 @@ class Column extends React.Component {
     }
 
     render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>
@@ -37,9 +33,9 @@ class Column extends React.Component {
               <Card key={cardData.id} {...cardData}/>
             ))}
 </div>
-        {/*  <div className={styles.creator}>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-          </div>*/}
+        <div className={styles.creator}>
+            <Creator text={settings.cardCreatorText} action={addCard}/>
+          </div>
         </section>
       );
     }
